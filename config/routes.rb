7dc36e957600c5415a2b5d56291455fca711e5e9
EC2 Default User <ec2-user@ip-172-31-38-167.ap-northeast-1.compute.internal>
2021-02-03 04:31:05 +0000

    20210203時点　ログイン機能（デバイス）追加途中

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # get "/tasks",to: "tasks#index"
   # post "/tasks",to: "tasks#create"
   # delete "/tasks/:id",to: "tasks#destroy"
   resources :tasks
+  
+  root "tasks#index"
+  get "tasks/show"
 end
