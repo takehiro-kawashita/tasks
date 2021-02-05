50bae774a948b18521e224b933516a660b51bd3f
EC2 Default User <ec2-user@ip-172-31-38-167.ap-northeast-1.compute.internal>
2021-02-05 22:22:24 +0000

    2021020５時点　最新データ

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,6 +4,10 @@ class TasksController < ApplicationController
         @task = Task.all
     end
     
+    def new
+        
+    end
+    
     def create
         @task = Task.new(limited_at: params[:limited_at] ,rank: params[:rank],title: params[:title],detail: params[:detail],category: params[:category])
         @result = @task.save
